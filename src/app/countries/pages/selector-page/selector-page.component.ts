3fed15e5c5c476a02a8b9d3f82fae4f9690cecb0
Juan <gurquiza8@gmail.com>
2024-05-01 15:16:46 -0500

    IMPORTANT... using switchmap for do it another petition. RXJS Operators

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/country.interfaces';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
   styles: ``
 })
-export class SelectorPageComponent {
+export class SelectorPageComponent implements OnInit{
 
   public myForm: FormGroup = this.fb.group({
     region: [ '', Validators.required ],
@@ -14,12 +17,33 @@ export class SelectorPageComponent {
     borders: [ '', Validators.required ],
   });
 
-  constructor( private fb: FormBuilder ) {}
+  constructor(
+    private fb: FormBuilder,
+    private countriesService: CountriesService
+  ) {}
+
+  ngOnInit(): void {
+    this.onRegionChanged();
+  }
+
+
+  get regions(): Region[] {
+    return this.countriesService.regions;
+  }
+
+  onRegionChanged(): void {
+    this.myForm.get('region')!.valueChanges
+      .pipe(
+        switchMap( region => this.countriesService.getCountriesByRegion(region) ),
+      )
+      .subscribe( value => {
+        console.log({ region: value});
+      });
+  }
 
   onSubmit() {
     if ( this.myForm.invalid ) return;
     console.log( this.myForm );
-
   }
 
 }
